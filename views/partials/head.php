6b2eecbb5978329074a76b822108caa02f63537d
sliman <ssoverlord04@gmail.com>
2024-12-08 10:33:23 +0100

    add menu for phones

--- views/partials/head.php
+++ views/partials/head.php
@@ -4,6 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Parkinsans:wght@300..800&display=swap"
+      rel="stylesheet"
+    />
+    <meta name="theme-color" content="#ffffff" />
+    <link
+      rel="shortcut icon"
+      href="https://avatars.githubusercontent.com/u/67262067?v=4"
+      type="image/x-icon"
+    />
     <title>Paw</title>
     <link rel="stylesheet" href="./src/css/add.css">
     <link rel="stylesheet" href="./src/css/nav.css">
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -5,7 +5,10 @@
         <input type="submit" class="buttonOrange" value="Update" formaction="./update<?= $obj ?>">
         <input type="submit" class="buttonRed" value="Delete" formaction="./delete<?= $obj ?>">
     </form>
+    <i class="fas fa-bars buttonMenu"></i>
     <ul>
+        <i class="fas fa-times buttonMenu"></i>
+        <li><a href="./">Home</a></li>
         <li><a href="./student">Student</a></li>
         <li><a href="./teacher">Teacher</a></li>
         <li><a href="./module">Module</a></li>
@@ -16,3 +19,18 @@
     </ul>
 
 </nav>
+<script>
+    const buttonMenu = document.getElementsByClassName("buttonMenu");
+    buttonMenu[0].addEventListener("click", function() {
+        document.querySelector("nav ul").classList.toggle("active");
+        document.querySelector("nav ul").style.display = "block";
+    })
+    buttonMenu[1].addEventListener("click", function() {
+        document.querySelector("nav ul").classList.toggle("active");
+        document.querySelector("nav ul").style.display = "none";
+    })
+</script>
+<script
+src="https://kit.fontawesome.com/2610c4262a.js"
+crossorigin="anonymous"
+></script>
